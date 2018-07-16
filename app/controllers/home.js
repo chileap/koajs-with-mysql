8e308f24bce8c8c129f575c2307ffed36da3d9ba
chileap <chileapchhin@gmail.com>
2018-07-16 10:43:54 +0700

    add search title and description into home page

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,6 +3,8 @@ import models from '../models/index';
 
 const index = async (ctx, _next) => {
   let page = parseInt(ctx.query.page, 10) || 1;
+  let queryTitle = ctx.query.title || ''
+  let queryDescription = ctx.query.description || ''
   page = page > 0 ? page : 1;
   let pageOffset = ( page - 1 ) * 10;
   const todos_query = {
@@ -10,17 +12,27 @@ const index = async (ctx, _next) => {
     order: [['createdAt', 'DESC']],
     offset: pageOffset,
     limit: 10,
+    where: {
+      title: {
+        $like: '%'+queryTitle+'%'
+      },
+      description: {
+        $like: '%'+queryDescription+'%'
+      }
+    }
   };
   const [todoCount, todos] = await Promise.all([
     models.Todo.count(),
     models.Todo.findAll(todos_query)
   ]);
+
   const locals = {
     title: 'Home',
     nav: 'index',
     todos: todos,
     baseUrl: '/',
     currentPage: page,
+    searchFormPath: '/',
     pages: parseInt(todoCount / 10 + 1)
   };
   await ctx.render('home/index', locals);
